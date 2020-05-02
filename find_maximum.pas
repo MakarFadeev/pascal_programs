5c200ff39c327d7cabcb0bfcf646879ba87f4a1b
Makar Fadeev <mafad2010@gmail.com>
2020-05-03 00:16:48 +0300

    add find_maximum.pas

---
+++ find_maximum.pas
@@ -0,0 +1,12 @@
+﻿program ooo;
+const n=5;
+var a:array[1..n] of integer; max:integer; f:integer;
+begin
+  for f:=1 to n do
+    readln (a[f]);
+  max:=a[1];
+  for f:=2 to n do
+    if a[f] > max then
+      max:=a[f];
+    writeln (max);
+end.

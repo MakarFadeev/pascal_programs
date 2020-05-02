32089db705d1edc282b16744ff2d526c66d8ffc7
Makar Fadeev <mafad2010@gmail.com>
2020-05-03 00:39:38 +0300

    add program count_divideable_by_two

---
+++ count_divideable_by_two.pas
@@ -0,0 +1,14 @@
+﻿program count_divideable_by_two;
+var a:array[0..5] of integer; b,c:integer;
+begin
+  randomize;
+  for c:=0 to 5 do
+    a[c]:=random (81)+20;
+  b:=0;
+  for c:=0 to 5 do
+    if a[c] mod 2=0 then
+      b:=b+1;
+  for c:=0 to 5 do
+    writeln (a[c]);
+  writeln ('divided by two: ',b);
+end.

e939314cefc5e0ff7c94e2ab0287d62ba1a7df25
Makar Fadeev <mafad2010@gmail.com>
2020-05-03 11:36:38 +0300

    add FooBar test solution

---
+++ FooBar.pas
@@ -0,0 +1,17 @@
+﻿program FooBar;
+var d,e:integer;
+begin
+  writeln ('введите, сколько чисел будет в ряде.');
+  read (d);
+  for e:=1 to d do
+    begin
+    if e mod 15 = 0 then
+ writeln ('FooBar')
+ else if e mod 3 = 0 then
+   writeln ('Foo')
+ else if e mod 5 = 0 then
+   writeln ('Bar')
+ else
+   writeln (e)
+ end;
+end.

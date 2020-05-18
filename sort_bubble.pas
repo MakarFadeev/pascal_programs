4f0a26f3838a744c69f15bafb3aab9b6969078d5
Makar Fadeev <mafad2010@gmail.com>
2020-05-18 20:39:29 +0300

    added sort_bubble

---
+++ sort_bubble.pas
@@ -0,0 +1,18 @@
+﻿const n=10;
+var a:array[1..n]of integer;b,c,d:integer;
+begin
+for d:=1 to n do
+begin
+a[d]:=random(1,1000);
+end;
+for d:=1 to n-1 do
+for c:=d+1 to n do
+if a[d]<a[c] then
+begin
+b:=a[d];
+a[d]:=a[c];
+a[c]:=b;
+end;
+for d:=1 to n do
+writeln(a[d]);
+end.
